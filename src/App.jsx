4be56222efc5144911506f61b1213d9806b1b585
EkramjemalH <ekramjemalh@gmail.com>
2025-07-16 18:59:53 +0300

    Add UserContext and refactor UserProfile to use Context API

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import UserProfile from "./components/UserProfile";
 import Footer from "./components/Footer";
+import UserContext from "./components/UserContext";
 
 function App() {
+  const userData = {
+    name: "Ekram Jemal",
+    age: 24,
+    bio: "Fitness enthusiast and React learner.",
+  };
+
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <UserProfile
-        name="Ekram Jemal"
-        age={24}
-        bio="Fitness enthusiast and React learner."
-      />
-      <Footer />
-    </div>
+    <UserContext.Provider value={userData}>
+      <div>
+        <Header />
+        <MainContent />
+        <UserProfile /> {/* No props needed now */}
+        <Footer />
+      </div>
+    </UserContext.Provider>
   );
 }
 
