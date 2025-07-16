2aac96aaac7e837f45c8e9025f9f66230d5a5e18
EkramjemalH <ekramjemalh@gmail.com>
2025-07-16 18:51:26 +0300

    Add Counter component and integrate it into App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter"; // Import the Counter component
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+      <Counter /> {/* Add the Counter component here */}
       <Footer />
       <WelcomeMessage />
     </>
