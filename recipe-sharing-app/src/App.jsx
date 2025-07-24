484c4ef84484f2de4faa5da2b9af296b5f6eed8b
EkramjemalH <ekramjemalh@gmail.com>
2025-07-24 10:05:34 +0300

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,9 +1,11 @@
 // src/App.jsx
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
-function App() {
+function Home() {
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
       <h1>Recipe Sharing App</h1>
@@ -11,6 +13,22 @@ function App() {
       <RecipeList />
     </div>
   );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+    </Routes>
+  );
+}
+
+// Helper wrapper to extract URL param and pass to RecipeDetails
+import { useParams } from "react-router-dom";
+function RecipeDetailsWrapper() {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={id} />;
 }
 
 export default App;
