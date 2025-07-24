b89e7d998ec04f87ae1c67f272f29740b62b6db4
EkramjemalH <ekramjemalh@gmail.com>
2025-07-24 10:11:52 +0300

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,34 +1,33 @@
 // src/App.jsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
-
-function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
-  );
-}
+import EditRecipeForm from "./components/EditRecipeForm";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-    </Routes>
+    <Router>
+      <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/edit/:id" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
-
-// Helper wrapper to extract URL param and pass to RecipeDetails
-import { useParams } from "react-router-dom";
-function RecipeDetailsWrapper() {
-  const { id } = useParams();
-  return <RecipeDetails recipeId={id} />;
 }
 
 export default App;
