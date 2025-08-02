a3078597066f3d5b8affaf96cb8e82aa6889beb2
EkramjemalH <ekramjemalh@gmail.com>
2025-08-02 11:25:57 +0300

    initial commit :github user search app setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>GitHub User Search</h1>
+      {/* Future: Add search input, result list */}
+    </div>
+  );
+}
+
+export default App;
