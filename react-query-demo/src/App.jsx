3125333a8403a7f90a6461e353699ece45308725
EkramjemalH <ekramjemalh@gmail.com>
2025-08-19 12:59:11 +0300

    update

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,25 +1,20 @@
-import React, { useState } from 'react'
-import PostsComponent from './components/PostsComponent'
+// src/App.jsx
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import PostsComponent from "./components/PostsComponent";
 
-export default function App() {
-  const [showPosts, setShowPosts] = useState(true)
+// Create a client
+const queryClient = new QueryClient();
 
+function App() {
   return (
-    <div style={{ padding: '1rem', fontFamily: 'system-ui, sans-serif' }}>
-      <h1>React Query Demo</h1>
-
-      <p>
-        Use the toggle to unmount/remount <code>PostsComponent</code> and see cached data load instantly
-        (no network) if within the <code>staleTime</code>.
-      </p>
-
-      <button onClick={() => setShowPosts((s) => !s)}>
-        {showPosts ? 'Hide' : 'Show'} Posts
-      </button>
-
-      <hr style={{ margin: '1rem 0' }} />
-
-      {showPosts ? <PostsComponent /> : <em>PostsComponent is unmounted</em>}
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
+  );
 }
+
+export default App;
